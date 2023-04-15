0cf2c1eb8a06b1865ad4c2dd8dee769157387d51
affmde <andrferr@student.hive.fi>
2023-04-15 12:12:39 +0300

    added move right and left. Keys fixed

--- srcs/mlx_handling/events.c
+++ srcs/mlx_handling/events.c
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 10:30:22 by andrferr          #+#    #+#             */
-/*   Updated: 2023/04/15 11:26:54 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/04/15 12:11:21 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,19 +68,54 @@ void	move_backwards(t_cub3d *cub3d)
 		cub3d->camera->y -= cub3d->camera->dir_y * MOVEMENT_SPEED;
 }
 
+void	move_left(t_cub3d *cub3d)
+{
+	int		x;
+	int		y;
+
+	y = (int)cub3d->camera->y;
+	x = (int)(cub3d->camera->x - cub3d->camera->plane_x * TEST_SPEED);
+	if (cub3d->map->map[y][x] != '1')
+		cub3d->camera->x -= cub3d->camera->plane_x * MOVEMENT_SPEED;
+	y = (int)(cub3d->camera->y - cub3d->camera->plane_x * TEST_SPEED);
+	x = (int)cub3d->camera->x;
+	if (cub3d->map->map[y][x] != '1')
+		cub3d->camera->y -= cub3d->camera->plane_y * MOVEMENT_SPEED;
+}
+
+void	move_right(t_cub3d *cub3d)
+{
+	int		x;
+	int		y;
+
+	y = (int)cub3d->camera->y;
+	x = (int)(cub3d->camera->x + cub3d->camera->plane_x * TEST_SPEED);
+	if (cub3d->map->map[y][x] != '1')
+		cub3d->camera->x += cub3d->camera->plane_x * MOVEMENT_SPEED;
+	y = (int)(cub3d->camera->y + cub3d->camera->plane_x * TEST_SPEED);
+	x = (int)cub3d->camera->x;
+	if (cub3d->map->map[y][x] != '1')
+		cub3d->camera->y += cub3d->camera->plane_y * MOVEMENT_SPEED;
+}
+
 int	deal_key(int keycode, t_cub3d *cub3d)
 {
+	ft_printf("keycode: %d\n", keycode);
 	if (keycode == 65307 || keycode == 53)
 	{
 		mlx_destroy_window(cub3d->ptr, cub3d->win);
 		exit (1);
 	}
-	else if (keycode == 65362 || keycode == 126)
+	else if (keycode == 119 || keycode == 126)
 		move_forward(cub3d);
+	else if (keycode == 115 || keycode == 125)
+		move_backwards(cub3d);
+	else if (keycode == 97)
+		move_left(cub3d);
+	else if (keycode == 100)
+		move_right(cub3d);
 	else if (keycode == 65363 || keycode == 124)
 		turn_right(cub3d);
-	else if (keycode == 65364 || keycode == 125)
-		move_backwards(cub3d);
 	else if (keycode == 65361 || keycode == 123)
 		turn_left(cub3d);
 	refresh_window(cub3d);

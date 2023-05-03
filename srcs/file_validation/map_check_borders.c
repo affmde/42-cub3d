0b23434f3d22d27dbf7ee0ea45ec033b33b89918
Andre Filipe Ferreira Miranda <andrferr@c2r5p13.hive.fi>
2023-05-03 13:25:45 +0300

    fixed 0s being accepted if they would not interfir in the map inbounds

--- srcs/file_validation/map_check_borders.c
+++ srcs/file_validation/map_check_borders.c
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 11:20:26 by andrferr          #+#    #+#             */
-/*   Updated: 2023/05/02 16:53:07 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/05/03 13:21:45 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,12 @@ int	check_borders(t_map_check *map)
 			{
 				if (map->map[i - 1][j] == '@' || map->map[i + 1][j] == '@'
 				|| map->map[i][j - 1] == '@' || map->map[i][j + 1] == '@')
+					return (1);
+			}
+			if (map->map[i][j] == '0')
+			{
+				if (map->map[i - 1][j] == ' ' || map->map[i + 1][j] == ' '
+				|| map->map[i][j - 1] == ' ' || map->map[i][j + 1] == ' ')
 					return (1);
 			}
 		}
